cb883a457f2898d642acbce79e3afca447bb065f
bsteve456 <stevealan1@protonmail.com>
2019-12-18 00:18:22 +0100

    add color

--- srcs/display_loop_rt.c
+++ srcs/display_loop_rt.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 10:40:22 by blacking          #+#    #+#             */
-/*   Updated: 2019/12/17 19:41:30 by blacking         ###   ########.fr       */
+/*   Updated: 2019/12/18 00:09:54 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ t_ray	init_ray(float i, float j, t_widw widw)
 	new_ray.dir.x = i - (widw.width / 2);
 	new_ray.dir.y = (widw.heigth / 2) - j;
 	new_ray.dir.z = (widw.heigth/2) / (tan((M_PI * 0.5 * 50) / 180.0));
-//	new_ray.dir.z = 40.1;
 	new_ray.dir = vectSub(new_ray.dir, new_ray.orig);
 	new_ray.dir = normalize(new_ray.dir);
 	return new_ray;
@@ -50,13 +49,17 @@ void	display_loop(data_t data, t_widw widw)
 	float i;
 	float j;
 	t_ray ray;
-
+	int r = 100;
+	int g = 0;
+	int b = 255;
+	int color =  r * 65536 + g * 256 + b;
+	printf("color : %d", color);
 	t_sphere sphere;
 
-	sphere.center.x = 0.0;
-	sphere.center.y = 0.0;
-	sphere.center.z = 62;
-	sphere.radius = 5.0;
+	sphere.center.x = 10.0;
+	sphere.center.y = -10.0;
+	sphere.center.z = 120;
+	sphere.radius = 20.0;
 	i = 0.0;
 	j = 0.0;
 	while(i <= widw.width)
@@ -65,11 +68,10 @@ void	display_loop(data_t data, t_widw widw)
 		while(j <= widw.heigth)
 		{
 			ray = init_ray(i, j, widw);
-//			printf("%f %f %f\n", ray.dir.x, ray.dir.y, ray.dir.z);
 			if(inter_sphere(sphere, ray) == 1)
 			{
 				printf("%f %f %f\n", ray.dir.x, ray.dir.y, ray.dir.z);
-				mlx_pixel_put(data.mlx_ptr, data.mlx_win, i, j, 170 + 5 + 12);
+				mlx_pixel_put(data.mlx_ptr, data.mlx_win, i, j, color);
 			}
 			j++;
 		}
