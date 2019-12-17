c647d8d95536736856eee9c31928aea3bdabc291
bsteve456 <stevealan1@protonmail.com>
2019-12-17 19:42:27 +0100

    add good sphere

--- minirt.c
+++ minirt.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 10:42:18 by blacking          #+#    #+#             */
-/*   Updated: 2019/12/17 16:51:56 by blacking         ###   ########.fr       */
+/*   Updated: 2019/12/17 19:28:29 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int main(void)
     data_t		data;
 	t_widw		window;
 
-	window.width = 300;
-	window.heigth = 300;
+	window.heigth = 1000;
+	window.width = 1000;
 	if ((data.mlx_ptr = mlx_init()) == NULL)
 		return (EXIT_FAILURE);
-	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, window.heigth, window.width, "Hello world")) == NULL)
+	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, window.width, window.heigth, "Hello world")) == NULL)
 		return (EXIT_FAILURE);
 	display_loop(data, window);
     mlx_loop(data.mlx_ptr);
--- srcs/display_loop_rt.c
+++ srcs/display_loop_rt.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 10:40:22 by blacking          #+#    #+#             */
-/*   Updated: 2019/12/17 17:12:55 by blacking         ###   ########.fr       */
+/*   Updated: 2019/12/17 19:41:30 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 t_ray	init_ray(float i, float j, t_widw widw)
 {
 	t_ray new_ray;
-	new_ray.orig.x = 5.0;
-	new_ray.orig.y = 5.0;
+	new_ray.orig.x = -10.0;
+	new_ray.orig.y = -10.0;
 	new_ray.orig.z = 0.0;
 	new_ray.dir.x = i - (widw.width / 2);
 	new_ray.dir.y = (widw.heigth / 2) - j;
-	new_ray.dir.z = (widw.heigth/2) / (tan(35 * 0.5));
+	new_ray.dir.z = (widw.heigth/2) / (tan((M_PI * 0.5 * 50) / 180.0));
+//	new_ray.dir.z = 40.1;
 	new_ray.dir = vectSub(new_ray.dir, new_ray.orig);
 	new_ray.dir = normalize(new_ray.dir);
 	return new_ray;
@@ -38,8 +39,7 @@ int		inter_sphere(t_sphere sphere, t_ray ray)
 	a = vectDot(ray.dir, ray.dir);
 	b = 2 * vectDot(ray.dir, vectOS);
 	c = vectDot(vectOS, vectOS) - (sphere.radius * sphere.radius);
-	discr = b * b - 4 * a * c;
-	printf("dicrm : %f\n", discr);
+	discr = (b * b) - (4 * a * c);
 	if(discr >= 0.0000)
 		return(1);
 	return (0);
@@ -53,9 +53,9 @@ void	display_loop(data_t data, t_widw widw)
 
 	t_sphere sphere;
 
-	sphere.center.x = 10.0;
-	sphere.center.y = 10.0;
-	sphere.center.z = 12.5;
+	sphere.center.x = 0.0;
+	sphere.center.y = 0.0;
+	sphere.center.z = 62;
 	sphere.radius = 5.0;
 	i = 0.0;
 	j = 0.0;
@@ -65,9 +65,10 @@ void	display_loop(data_t data, t_widw widw)
 		while(j <= widw.heigth)
 		{
 			ray = init_ray(i, j, widw);
-			printf("%f %f %f\n", ray.dir.x, ray.dir.y, ray.dir.z);
+//			printf("%f %f %f\n", ray.dir.x, ray.dir.y, ray.dir.z);
 			if(inter_sphere(sphere, ray) == 1)
 			{
+				printf("%f %f %f\n", ray.dir.x, ray.dir.y, ray.dir.z);
 				mlx_pixel_put(data.mlx_ptr, data.mlx_win, i, j, 170 + 5 + 12);
 			}
 			j++;
