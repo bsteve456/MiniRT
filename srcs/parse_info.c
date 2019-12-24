16b8adab4de9922216c3f398be8e950aa0882a5b
bsteve456 <stevealan1@protonmail.com>
2019-12-24 17:08:20 +0100

    check some errors

--- srcs/parse_info.c
+++ srcs/parse_info.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/24 16:29:27 by blacking          #+#    #+#             */
-/*   Updated: 2019/12/24 17:05:24 by blacking         ###   ########.fr       */
+/*   Updated: 2019/12/24 17:07:52 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	parse_info_scene(t_list *scene, char *line)
 	if(ft_strncmp(line, "R", 2) == 0 ||
 		ft_strncmp(line, "A", 2) == 0 ||
 		ft_strncmp(line, "c", 2) == 0)
-		scene_env(scene, line + 2);
+		scene_env(scene, line);
 	else if(ft_strncmp(line, "sp", 2) == 0)
-		scene_object(scene, line + 2);
+		scene_object(scene, line);
 }
