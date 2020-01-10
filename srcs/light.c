42967fbaac06951dc0cdfc93d115e07411b3be34
bsteve456 <stevealan1@protonmail.com>
2020-01-10 18:15:02 +0100

    test color of light

--- srcs/light.c
+++ srcs/light.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 14:54:49 by blacking          #+#    #+#             */
-/*   Updated: 2020/01/10 17:27:36 by blacking         ###   ########.fr       */
+/*   Updated: 2020/01/10 18:14:11 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ void	light(vect Pt, t_sphere *sphere, data_t *data, t_light *light)
 	L = normalize(L);
 //	printf("N: %f, %f, %f\n", N.x, N.y, N.z);
 //	printf("L :%f, %f, %f\n", L.x, L.y, L.z);
-	surface_color = light->color * light->ratio* (-1 * (floor(vectDot(N, L)*10)/10));
+	surface_color = (light->color * light->ratio) * (-1 * (floor(vectDot(N, L)*5)/5));
 //	printf("N: %f, %f, %f\n", N.x, N.y, N.z);
 //	printf("L :%f, %f, %f\n", L.x, L.y, L.z);
-	printf("NL : %f\n", floor(vectDot(N, L)*10)/10);
+//	printf("NL : %f\n", floor(vectDot(N, L)*10)/10);
+	if(surface_color < 0.0)
+		surface_color *= 0.0;
+	printf("NL : %f\n", floor(vectDot(N, L)*5/5));
 	printf("color : %f\n", surface_color);
 	mlx_pixel_put(data->mlx_ptr, data->mlx_win, data->x, data->y, surface_color);
 }
