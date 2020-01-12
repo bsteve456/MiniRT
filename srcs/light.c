b6df312311d40934687196c6e2ccad7248ca9a1f
bsteve456 <stevealan1@protonmail.com>
2020-01-12 10:37:30 +0100

    simplify code

--- srcs/light.c
+++ srcs/light.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 14:54:49 by blacking          #+#    #+#             */
-/*   Updated: 2020/01/12 10:31:51 by blacking         ###   ########.fr       */
+/*   Updated: 2020/01/12 10:36:57 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,14 @@ void	light(vect Pt, t_sphere *sphere, data_t *data, t_light *light)
 	vect L;
 	int surface_color;
 
-//	sphere->center = normalize(sphere->center);
-//	light->orig = normalize(light->orig);
 	N = vectSub(Pt, sphere->center);
 	L = vectSub(light->orig, Pt);
-//	L = vectMult(data->ray.dir, 1);
 	N = normalize(N);
 	L = normalize(L);
-//	float a = vectDot(N, L);
-//	int b = a * 255;
-//	printf("N: %f, %f, %f\n", N.x, N.y, N.z);
-//	printf("L :%f, %f, %f\n", L.x, L.y, L.z);
-//	surface_color = (light->color * light->ratio) * vectDot(N, L);
-//	surface_color = mlx_get_color_value(data->mlx_ptr ,pow(256, 2)* b + pow(256, 1) * b + b);
 	surface_color = reflected_color(light, vectDot(N,L));
-//	printf("N: %f, %f, %f\n", N.x, N.y, N.z);
-//	printf("L :%f, %f, %f\n", L.x, L.y, L.z);
-//	printf("NL : %f\n", floor(vectDot(N, L)*10)/10);
 	if(surface_color < 0.0)
-		surface_color *= 0.0;	
-//	printf("NL : %f\n", vectDot(N, L));
-//	printf("color : %f\n", surface_color);
-//	if(surface_color > 255 * 2)
-		mlx_pixel_put(data->mlx_ptr, data->mlx_win, data->x, data->y, surface_color);
+		surface_color *= 0.0;
+	mlx_pixel_put(data->mlx_ptr, data->mlx_win, data->x, data->y, surface_color);
 }
 
 void	light_loop(float t0, t_sphere *sphere, data_t *data, t_list *scene)
@@ -61,8 +46,6 @@ void	light_loop(float t0, t_sphere *sphere, data_t *data, t_list *scene)
 	vect Pt;
 	object *t_light;
 	Pt = vectAdd(data->ray.orig, vectMult(data->ray.dir, t0));
-//	Pt = normalize(Pt);
-//	printf("Pt :%f, %f, %f\n", Pt.x, Pt.y, Pt.z);
 	while(scene)
 	{
 		t_light = scene->content;
