fe1f8be43e784876495085211bdcc61e87242c2e
bsteve456 <stevealan1@protonmail.com>
2020-01-16 11:07:25 +0100

    add a function to simplify code

--- srcs/sphere_plane_inter.c
+++ srcs/sphere_plane_inter.c
@@ -1,16 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   obj_verif.c                                        :+:      :+:    :+:   */
+/*   sphere_plane_inter.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/30 09:47:34 by blacking          #+#    #+#             */
-/*   Updated: 2020/01/15 14:38:27 by blacking         ###   ########.fr       */
+/*   Updated: 2020/01/16 11:05:51 by blacking         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
+
+
+void	sphere_n_pt(float t0, t_sphere *sphere, data_t *data, t_list *scene)
+{
+	vect Pt;
+	vect N;
+
+	Pt = vectAdd(data->ray.orig, vectMult(data->ray.dir, (double)t0));
+	N = vectSub(Pt, sphere->center);
+	Pt = vectMult(vectAdd(Pt, N), 0.9);
+	data->rgb = sphere->rgb;
+	if(shadow_ray(scene, Pt, data) == 0)
+		light_loop(Pt, N, data, scene);
+}
 
 float find_t0(float a, float b, float c)
 {
@@ -45,31 +59,28 @@ void	inter_sphere(t_sphere *sphere, data_t *data, t_list *scene)
 	if((b * b) - (4 * a * c) >= 0.0000)
 	{
 		a = find_t0(a, b, c);
-		vect Pt = vectAdd(data->ray.orig, vectMult(data->ray.dir, (double)a));
-		vect N = vectSub(Pt, sphere->center);
-		Pt = vectMult(vectAdd(Pt, N), 0.9);
-		data->rgb = sphere->rgb;
-		if(shadow_ray(scene, Pt, data) == 0)
-			light_loop(Pt, N, data, scene);
+		sphere_n_pt(a, sphere, data, scene);
 	}
 }
 
 void	inter_plane(t_plane *plane, data_t *data, t_list *scene)
 {
 	int color;
+	vect Pt;
+	vect p0l0;
+	float t;
+	float demom;
 
 	color = plane->rgb.r * pow(256, 2) + plane->rgb.g * 256 + plane->rgb.b;
 	plane->N = plane->N;
-	float demom = vectDot(data->ray.dir, plane->N);
+	demom = vectDot(data->ray.dir, plane->N);
 	if(fabs(demom) > 0.00001f) {
-		vect p0l0 = vectSub(plane->p0, data->ray.orig);
-		float t = vectDot(p0l0, plane->N) / demom;
-
+		p0l0 = vectSub(plane->p0, data->ray.orig);
+		t = vectDot(p0l0, plane->N) / demom;
 		if(t >= 0.00001f)
 		{
-			vect Pt = vectAdd(data->ray.orig, vectMult(data->ray.dir, t));
+			Pt = vectAdd(data->ray.orig, vectMult(data->ray.dir, t));
 			data->rgb = plane->rgb;
-
 			if(shadow_ray(scene, Pt, data) == 0)
 				mlx_pixel_put(data->mlx_ptr, data->mlx_win, data->x, data->y, color);
 		}
